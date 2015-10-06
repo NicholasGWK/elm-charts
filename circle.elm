f3a99f606d95d8b083f52c73af2fa6c39c515c1d
NicholasGW <nicholasgkennedy@gmail.com>
2015-10-06 20:24:31 -0300

    Some stuff

--- circle.elm
+++ circle.elm
@@ -10,10 +10,35 @@ import Debug exposing (..)
 
 main =
   collage 200 200 [
-  (traced (solid red) (List.map ( arcSegmentToBezierCurve (0,pi/4) 100) (steps 100))),
-  (traced (solid blue) (List.map ( arcSegmentToBezierCurve (pi/4,pi) 100) (steps 100)))]
+  (traced (solid red) arcToPath (0,pi/4) 100),
+  ]
 
-arcSegmentToBezierCurve arc radius =
+
+colors = [ red , blue , yellow , green , lightRed, lightGreen, lightYellow]
+
+arcToPath: (Float, Float) -> Float -> Path
+arcToPath arc radius =
+  (List.map (arcSegmentToBezierCurve arc radius) (steps 100)) |> path
+
+
+normalize: List Float -> List Float
+normalize dataset =
+    List.map (\data -> data * pi / List.sum(dataset)) dataset
+
+calcArcLengths: List Float -> List (Float,Float)
+calcArcLengths normalizedData =
+      foldl addToList [] normalizedData
+
+addToList: Float -> List (Float,Float) -> List (Float,Float)
+addToList x xs =
+    case xs of
+      [] ->
+        [(0,x)]
+
+      (v1,v2)::t1 -> (v2,v2 + x)::xs
+
+
+arcSegmentToBezierCurveEquation arc radius =
  computeBezier ( fromPolar (radius, fst arc)) (fromPolar (computeControlPoint (radius, fst arc) (calculateC arc) (pi / 2) )) (fromPolar(computeControlPoint (radius,snd arc) (calculateC arc) (pi / 2 * -1))) (fromPolar(radius, snd arc))
 
 
@@ -23,7 +48,7 @@ scalePolar point m =
 
 
 calculateC arc =
-  log "c" ((4/3) * tan (pi / (2*( ((2 * pi)/((snd arc) - fst (arc)) )))))
+ ((4/3) * tan (pi / (2*( ((2 * pi)/((snd arc) - fst (arc)) )))))
 
 computeBezier p0 p1 p2 p3 t =
    ( (1-t)^3 * fst p0 + 3 * (1-t)^2 * t * fst p1 + 3 * (1-t) * t^2 * fst p2 + t^3 * fst p3,
@@ -34,7 +59,7 @@ steps num =
 
 computeControlPoint: (Float,Float) -> Float -> Float -> (Float,Float)
 computeControlPoint p0 c rotation =
-  toPolar (addPolar p0 (log "rotate"(c*100,((snd p0) + rotation))))
+  toPolar (addPolar p0 ((c*100,((snd p0) + rotation))))
 
 addPolar: (Float,Float) -> (Float,Float) -> (Float,Float)
 addPolar p1 p2 =
